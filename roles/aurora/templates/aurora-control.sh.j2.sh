3fbb354037d00063650b86d7b07a5de6d252175a
Steve Salevan <steve.salevan@gmail.com>
2014-12-30 15:56:19 -0500

    Refactors and work to support aurora

---
+++ roles/aurora/templates/aurora-control.sh.j2.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# {{ ansible_managed }}
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Location where aurora-scheduler.zip was unpacked.
+AURORA_SCHEDULER_HOME=/usr/local/aurora-scheduler
+
+# Flags that control the behavior of the JVM.
+JAVA_OPTS=(
+  -server
+  # Location of libmesos-XXXX.so / libmesos-XXXX.dylib
+  -Djava.library.path=/usr/local/lib
+
+{% for jvm_flag in aurora_jvm_flags %}
+  {{ jvm_flag }}
+{% endfor %}
+)
+
+# Flags control the behavior of the Aurora scheduler.
+# For a full list of available flags, run bin/aurora-scheduler -help
+AURORA_FLAGS=(
+  -cluster_name='{{ aurora_cluster_name }}'
+
+  # Ports to listen on.
+  -http_port={{ aurora_http_port }}
+
+  -native_log_quorum_size='{{ aurora_native_log_quorum_size }}'
+
+  -zk_endpoints=localhost:2181
+  -mesos_master_address=$(cat /etc/aurora/mesos-zk)
+
+  -serverset_path='{{ aurora_serverset_path }}'
+
+  -native_log_zk_group_path='{{ aurora_native_log_zk_path }}'
+
+  -native_log_file_path="$AURORA_SCHEDULER_HOME/db"
+  -backup_dir="$AURORA_SCHEDULER_HOME/backups"
+
+  -thermos_executor_path=/dev/null
+  -gc_executor_path=/dev/null
+
+  -vlog='{{ aurora_log_level }}'
+  -logtostderr
+
+  # Extra flags:
+{% for extra_flag in aurora_extra_flags %}
+  {{ extra_flag }}
+{% endfor %}
+)
+
+# Environment variables control the behavior of the Mesos scheduler driver (libmesos).
+export GLOG_v=0
+export LIBPROCESS_PORT={{ aurora_libprocess_port }}
+
+JAVA_OPTS="${JAVA_OPTS[*]}" exec "$AURORA_SCHEDULER_HOME/bin/aurora-scheduler" "${AURORA_FLAGS[@]}"
